e2d0c34870283444521d7b01c24a6c210acff7e8
hamidreza sadeghi <xhamid0011@gmail.com>
2019-04-18 17:33:46 +0430

    server added

---
+++ server.py
@@ -0,0 +1,375 @@
+import torndb
+import tornado.escape
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import os
+from binascii import hexlify
+from tornado.options import define, options
+
+define("port", default=1104, help="run on the given port", type=int)
+define("mysql_host", default="127.0.0.1:3306", help="database host")
+define("mysql_database", default="test", help="database name")
+define("mysql_user", default="x", help="database user")
+define("mysql_password", default="y", help="database password")
+
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            #get
+             (r"/login/([^/]+)/([^/]+)", login),
+             (r"/logout/([^/]+)/([^/]+)", logout),
+             (r"/sendticket/([^/]+)/([^/]+)/([^/]+)", sendticket),
+             (r"/getticketcli/([^/]+)", getticketcli),
+             (r"/closeticket/([^/]+)/([^/]+)", closeticket),
+             (r"/getticketmod/([^/]+)", getticketmod),
+             (r"/restoticketmod/([^/]+)/([^/]+)/([^/]+)", restoticketmod),
+          #   (r"/changestatus/([^/]+)/([^/]+)", changestatus),
+             (r"/signup/([^/]+)/([^/]+)/([^/]+)/([^/]+)/([^/]+)", signup),
+            #post
+            (r"/login", login),
+            (r"/signup", signup),
+            (r"/logout", logout),
+            (r"/sendticket", sendticket),
+            (r"/getticketcli", getticketcli),
+            (r"/closeticket", closeticket),
+            (r"/getticketmod", getticketmod),
+            (r"/restoticketmod", restoticketmod),
+        #    (r"/changestatus", changestatus),
+            (r".*", defaulthandler),
+        ]
+        settings = dict()
+        super(Application, self).__init__(handlers, **settings)
+        self.db = torndb.Connection(
+            host=options.mysql_host, database=options.mysql_database,
+            user=options.mysql_user, password=options.mysql_password)
+
+
+class BaseHandler(tornado.web.RequestHandler):
+    @property
+    def db(self):
+        return self.application.db
+
+    def check_user(self, user):
+        resuser = self.db.get("SELECT * from user where username = %s", user)
+        if resuser:
+            return True
+        else:
+            return False
+
+    def check_api(self, api):
+        resuser = self.db.get("SELECT * from user where token = %s", api)
+        if resuser:
+            return True
+        else:
+            return False
+
+    def return_user(self, api):
+        resuser = self.db.get("SELECT * from user where token = %s", api)
+        if resuser:
+            return resuser
+        else:
+            return False
+
+    def check_auth(self,username,password):
+        resuser = self.db.get("SELECT * from user where username = %s and password = %s", username,password)
+        if resuser:
+            return True
+        else:
+            return False
+
+class defaulthandler(BaseHandler):
+    def get(self):
+        output = {'status': 'Wrong Command'}
+        self.write(output)
+
+    def post(self):
+        output = {'status': 'Wrong Command'}
+        self.write(output)
+
+
+class signup(BaseHandler):
+    def get(self,*args):
+        t = self.check_user(args[0])
+        if not t:
+            api_token = str(hexlify(os.urandom(16)))
+            user_id = self.db.execute("""INSERT INTO user (username, password,firstname,lastname, token,rule) values (%s,%s,%s,%s,%s,%s) """, args[0],args[1],args[2],args[3], api_token,args[4])
+
+            output = {'message': 'signed up Successfully',
+                      'code': '200',
+                      'api': api_token,
+                      'status': 'OK'}
+            self.write(output)
+        else:
+            output = {'status': 'User Exist'}
+            self.write(output)
+
+    def post(self, *args, **kwargs):
+        username = self.get_argument('username')
+        password = self.get_argument('password')
+        firstname = self.get_argument('firstname')
+        lastname = self.get_argument('lastname')
+        rule = self.get_argument('rule')
+        t = self.check_user(username)
+        if not t:
+            api_token = str(hexlify(os.urandom(16)))
+            user_id = self.db.execute(
+                """INSERT INTO user (username, password,firstname,lastname, token,rule) values (%s,%s,%s,%s,%s,%s) """,
+                username, password, firstname, lastname, api_token, rule)
+
+            output = {'api': api_token,
+                      'status': 'OK'}
+            self.write(output)
+        else:
+            output = {'status': 'User Exist'}
+            self.write(output)
+
+
+class login(BaseHandler):
+    def get(self, *args):
+        t = self.check_auth(args[0], args[1])
+        if t:
+            api_token = str(hexlify(os.urandom(16)))
+            user_id = self.db.execute("""update user set token = %s where username= %s""", api_token,args[0])
+
+            output = {'message': 'logged in Successfully',
+                      'code': '200',
+                      'api': api_token,
+                      'status': 'OK'}
+            self.write(output)
+        else:
+            output = {'status': 'User Exist'}
+            self.write(output)
+
+    def post(self):
+        username = self.get_argument('username')
+        password = self.get_argument('password')
+        t = self.check_auth(username, password)
+        if t:
+            api_token = str(hexlify(os.urandom(16)))
+            user_id = self.db.execute("""update user set token = %s where username= %s""", api_token, username)
+
+            output = {'api': api_token,
+                      'status': 'OK'}
+            self.write(output)
+        else:
+            output = {'status': 'User Exist'}
+            self.write(output)
+
+
+class logout(BaseHandler):
+    def get(self,*args):
+        t = self.check_auth(args[0], args[1])
+        if t:
+            api_token = ""
+            user_id = self.db.execute("""update user set token = %s where username= %s""", api_token,args[0])
+
+            output = {'message': 'logged out Successfully',
+                      'code': '200',
+                      'status': 'OK'}
+            self.write(output)
+        else:
+            output = {'status': 'Wrong username or password'}
+            self.write(output)
+
+    def post(self, *args, **kwargs):
+        username = self.get_argument('username')
+        password = self.get_argument('password')
+        t = self.check_auth(username, password)
+        if t:
+            api_token = ""
+            user_id = self.db.execute("""update user set token = %s where username= %s""", api_token, username)
+
+            output = {'message': 'logged out Successfully',
+                      'code': '200',
+                      'status': 'OK'}
+            self.write(output)
+        else:
+            output = {'status': 'Wrong username or password'}
+            self.write(output)
+
+
+class sendticket(BaseHandler):
+    def get(self,*args):
+        t = self.check_api(args[0])
+        sender = self.return_user(args[0])
+        if t:
+            user_id = self.db.execute("""INSERT INTO tickets (subject, body,situation,sender,reciver) values (%s,%s,%s,%s,%s) """, args[1], args[2], "open",sender["username"],sender["username"])
+
+            output = {'message': 'ticket sent Succsessfully',
+                      'code': '200',
+                      'status': 'OK'}
+            self.write(output)
+        else:
+            output = {'status': 'Wrong Api'}
+            self.write(output)
+
+    def post(self, *args, **kwargs):
+            token = self.get_argument('token')
+            subject = self.get_argument('subject')
+            body = self.get_argument('body')
+            t = self.check_api(token)
+            if t:
+                user_id = self.db.execute("""INSERT INTO tickets (subject, body,situation) values (%s,%s,%s) """,
+                                          subject, body, "open")
+
+                output = {'message': 'ticket sent Succsessfully',
+                          'code': '200',
+                          'status': 'OK'}
+                self.write(output)
+            else:
+                output = {'status': 'Wrong Api'}
+                self.write(output)
+
+
+class getticketcli(BaseHandler):
+    def get(self, *args):
+        t = self.check_api(args[0])
+        user = self.return_user(args[0])
+        print (user["username"])
+        if t:
+            user_id = self.db.query("SELECT * from tickets where reciver=%s", user["username"])
+            for x in user_id:
+                self.write(x)
+            output = {'message': 'ticket sent Succsessfully',
+                      'code': '200',
+                      'status': 'OK'}
+            self.write(output)
+        else:
+            output = {'status': 'Wrong Api'}
+            self.write(output)
+
+    def post(self):
+            token = self.get_argument('token')
+            t = self.check_api(token)
+            user = self.return_user(token)
+            print (user["username"])
+            if t:
+                user_id = self.db.query("""select * from tickets where reciver=%s""", user["username"])
+                for x in user_id :
+                    self.write(x)
+                output = {'message': 'ticket sent Succsessfully',
+                          'code': '200',
+                          'status': 'OK'}
+                self.write(output)
+            else:
+                output = {'status': 'Wrong Api'}
+                self.write(output)
+
+
+class closeticket(BaseHandler):
+    def get(self,*args):
+        # token = self.get_argument('token')
+        # id_2 = self.get_argument('id'
+        t = self.check_api(args[0])
+        user = self.return_user(args[0])
+        x = "close"
+        if t:
+            user_id = self.db.execute("""update tickets set situation =%s where id=%s """,x,args[1])
+            output = {'message': 'ticket sent Succsessfully',
+                      'code': '200',
+                      'status': 'OK'}
+            self.write(output)
+        else:
+            output = {'status': 'Wrong Api'}
+            self.write(output)
+
+    def post(self):
+        token = self.get_argument('token')
+        id_2 = self.get_argument('id')
+        t = self.check_api(token)
+        sender = self.return_user(token)
+        if t:
+            user_id = self.db.execute("""update tickets set subject = %s where id= %s""", "close", id_2[0])
+            output = {'message': 'ticket sent Succsessfully',
+                      'code': '200',
+                      'status': 'OK'}
+            self.write(output)
+        else:
+            output = {'status': 'Wrong Api'}
+            self.write(output)
+
+
+class getticketmod(BaseHandler):
+    def get(self, *args):
+        user = self.return_user(args[0])
+        y = "admin"
+        if user["rule"] == y:
+            user_id = self.db.query("SELECT * from tickets ")
+            for x in user_id:
+                self.write(x)
+            output = {'message': 'ticket sent Succsessfully',
+                      'code': '200',
+                      'status': 'OK'}
+            self.write(output)
+        else:
+            output = {'status': 'Wrong Api'}
+            self.write(output)
+
+    def post(self):
+            token = self.get_argument('token')
+            t = self.check_api(token)
+            user = self.return_user(token)
+            y = "admin"
+            if user["rule"] == y:
+                user_id = self.db.query("""select * from tickets """)
+                for x in user_id :
+                    self.write(x)
+                output = {'message': 'ticket sent Succsessfully',
+                          'code': '200',
+                          'status': 'OK'}
+                self.write(output)
+            else:
+                output = {'status': 'Wrong Api'}
+                self.write(output)
+
+
+class restoticketmod(BaseHandler):
+    def get(self,*args):
+        user = self.return_user(args[0])
+        y = "admin"
+        if user["rule"] == y:
+            user_id = self.db.get("""select sender from tickets  where id=%s """, args[1])
+            print (user_id)
+            user_id2= self.db.execute("""insert into tickets (sender,reciver,body,situation) values (%s,%s,%s,%s) ""","admin",user_id,args[2],"open")
+
+            output = {'message': 'ticket sent Succsessfully',
+                      'code': '200',
+                      'status': 'OK'}
+            self.write(output)
+        else:
+            output = {'status': 'Wrong Api'}
+            self.write(output)
+
+    def post(self, *args, **kwargs):
+            token = self.get_argument('token')
+            id_2 = self.get_argument('id')
+            body = self.get_argument('body')
+            t = self.return_user(token)
+            y = "admin"
+            if t["rule"] == y:
+                user_id = self.db.get("""select sender from tickets  where id=%s """, id_2)
+                user_id2 = self.db.execute(
+                    """insert into tickets (sender,reciver,body,situation) values (%s,%s,%s,%s) """, "admin", user_id,
+                    body, "open")
+
+                output = {'message': 'ticket sent Succsessfully',
+                          'code': '200',
+                          'status': 'OK'}
+                self.write(output)
+            else:
+                output = {'status': 'Wrong Api'}
+                self.write(output)
+
+
+
+def main():
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
+
+
+if __name__ == "__main__":
+    main()
